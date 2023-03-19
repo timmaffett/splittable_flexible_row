64d9eb4ec54cfa5fa750742359a23e31b68eff3c
Tim Maffett <timmaffett@gmail.com>
2023-03-19 13:44:45 -0700

    dart analyze

--- test/splittable_flexible_row_test.dart
+++ test/splittable_flexible_row_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:splittable_flexible_row/splittable_flexible_row.dart';
 
 void main() {
-  test('adds one to input values', () {
+  Splittable.splitWidth = 500;
+  test('add some test tim!', () {
     //final calculator = Calculator();
     //expect(calculator.addOne(2), 3);
     //expect(calculator.addOne(-7), -6);
